ee06f2bbdc2c70eec8ead20fc7066fc19bcac26b
127AWang <127awang@frhsd.com>
2025-09-08 10:34:35 -0400

    67 says hello to amergomma

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
             System.out.println("Hi my name is Amer");
+            System.out.println("Hi my name is Alston");
     }
 }
